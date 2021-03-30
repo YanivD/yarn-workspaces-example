f979232308009c8b555505cbe5a63e82129af98f
Yaniv Davidi <yaniv.davi@gmail.com>
2021-03-30 18:49:16 +0300

    Update index.js

--- packages/common/index.js
+++ packages/common/index.js
@@ -4,4 +4,5 @@ module.exports = () => {
   console.log("hello from common 12");
   console.log("hello from common 12");
   console.log("hello from common 12");
+  console.log("hello from common 12");
 };
